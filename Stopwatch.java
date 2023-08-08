7618b0895a909aaf764d40fbd7ab220e129da80f
Stefaaan <89779209+Stefaaan06@users.noreply.github.com>
2023-08-08 11:33:27 +0200

    uploaded files

---
+++ Stopwatch.java
@@ -0,0 +1,22 @@
+public class Stopwatch {
+    private static long startTime;
+    private static long endTime;
+
+    public static void start() {
+        startTime = System.currentTimeMillis();
+    }
+
+    public static void stop() {
+        endTime = System.currentTimeMillis();
+    }
+
+    public static long getElapsedTimeInMillis() {
+        Stopwatch.stop();
+        return endTime - startTime;
+    }
+
+    public static double getElapsedTimeInSeconds() {
+        Stopwatch.stop();
+        return (endTime - startTime) / 1000.0;
+    }
+}
